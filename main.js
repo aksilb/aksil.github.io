2c7c8464f4943101869eb2283f331c661db294dc
Abdelghani Benhaoui <66739622+aksilb@users.noreply.github.com>
2020-07-19 13:36:39 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+// window.sr = scrollBy();
+
+// scrollBy.apply(".animate-right", {
+//     origin: "left",
+//     duration: 1000,
+//     distance: "24rem",
+//     delay: 300,
+// });
+// sr.reveal(".animate-right", {
+//     origin: "right",
+//     duration: 1000,
+//     distance: "24rem",
+//     delay: 600,
+// });
+// sr.reveal(".animate-top", {
+//     origin: "top",
+//     duration: 1000,
+//     distance: "24rem",
+//     delay: 600,
+// });
+// sr.reveal(".animate-bottom", {
+//     origin: "bottom",
+//     duration: 1000,
+//     distance: "24rem",
+//     delay: 600,
+// });
